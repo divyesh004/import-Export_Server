b4cf9657af82956c56563ba654f20d2859fdd919
Divyesh <143060811+divyesh004@users.noreply.github.com>
2025-04-15 21:38:28 +0530

    Update mistral.service.js

--- Server/services/mistral.service.js
+++ Server/services/mistral.service.js
@@ -68,7 +68,7 @@ Product: ${productData.name}\nDescription: ${productData.description}\nCategory:
             }
           ],
           temperature: 0.7,
-          max_tokens: 2000
+          max_tokens: 20000
         },
         {
           headers: {
